8ab2d7d34f0489dc01b6539f3a3b4e026c4b4deb
Jsvier <gerardo.etchepare@gmail.com>
2018-12-09 18:12:19 -0300

    correcion de las imagens

--- src/Containers/HomeScreen/index.js
+++ src/Containers/HomeScreen/index.js
@@ -1,47 +1,64 @@
 import React, { Component } from 'react'
+import {StyleSheet, View, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { StyleSheet, View, Text } from 'react-native'
+import CardModal from '../../Components/card-modal';
 
-import CustomButton from '../../Components/CustomButton'
-
-/**
- * Just a centered logout button.
- */
 export default class HomeScreen extends Component {
-  static propTypes = {
-    logout: PropTypes.func
+  constructor(props) {
+    super(props);
+    this.state = {
+        scroll: true,
+    }
   }
+
   static navigationOptions = {
-    title: "Makro"
+    title: "Menu de aplicaciones de Makro",
+    headerTitleStyle :{textAlign: 'center',alignSelf:'center'}
   };
   render () {
     return (
-      <View style={styles.container}>
-        <Text>{this.props.navigation.state.params.name}</Text>
-        <CustomButton
-          text={'Logout'}
-          onPress={this.props.logout}
-          buttonStyle={styles.button}
-          textStyle={styles.buttonText}
-        />
+       <View style={styles.container}>
+         <ScrollView scrollEnabled={this.state.scroll} style={styles.container}>
+                <CardModal title={'Aereos'}
+                           description={'Sistema de agrupamiento y posicionamientos de articulos, desde rececpcion de mercaderias.'}
+                           image={require('../../Images/overhead-logo.png')}
+                           color='rgba(236,25,40,0.9)'
+                           content={'Simple idea de pallet.'}
+                           due={1}
+                />
+                <CardModal title={'Verificador de precio'}
+                           description={'Sistema para la busqueda e impresion de articulos activos'}
+                           image={require('../../Images/pricecheck-logo.png')}
+                           color='rgba(236,25,40,0.9)'
+                           content={'Simple idea de pricheck.'}
+                           due={2}
+                />
+                <CardModal title={'Hipersensible/Sensibles'}
+                           description={'Articulos sensible de rotacion dentro de las tiendas'}
+                           image={require('../../Images/sensitive-logo.png')}
+                           color='rgba(236,25,40,0.9)'
+                           content={'Simple idea de pricecheck.'}
+                           due={1}
+                />
+            </ScrollView>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+      flex: 1,
+      backgroundColor: '#ddd',
+      paddingTop: 20
+  },
+  box: {
+      backgroundColor: 'red'
   },
   button: {
-    backgroundColor: '#1976D2',
-    margin: 20
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold'
+      borderColor: 1,
+      borderWidth: 1,
   }
 })
+
